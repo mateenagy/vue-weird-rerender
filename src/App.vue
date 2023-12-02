e8b4597e9d61085c63e50de4ac91c1a52dee085a
Nagy Máté <nagy.mate.0125@gmail.com>
2023-12-02 12:34:37 +0100

    example

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Test from './components/Test.vue'
+
+const show = ref<boolean>(true);
+</script>
+<template>
+	<div>
+		<div>
+			<span>When I remove foo the onBeforeUnmount event log `bar_bad`</span>
+			<Test name="foo_bad" />
+			<Test name="bar_bad" />
+		</div>
+		<!-- <div>
+			<span>When I remove foo the onBeforeUnmount event log `foo_good`</span>
+			<Test name="foo_good" />
+			<div></div>
+			<Test name="bar_good" />
+		</div> -->
+		<!-- <div>
+			<span>When I remove foo the onBeforeUnmount event log `foo_good`</span>
+			<Test name="foo_good" v-if="show" />
+			<Test name="bar_good" />
+		</div> -->
+		<!-- <div>
+			<span>When I remove foo the onBeforeUnmount event log `foo_good`</span>
+			<div>
+				<Test name="foo_good" />
+			</div>
+			<Test name="bar_good" />
+		</div> -->
+		<!-- <div>
+			<span>When I remove foo the onBeforeUnmount event log `foo_good`</span>
+			<Test name="foo_good" :key="Math.random()" />
+			<Test name="bar_good" />
+		</div> -->
+	</div>
+</template>
---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { onBeforeUnmount } from 'vue';
+
+const props = defineProps<{
+	name: string;
+}>();
+
+onBeforeUnmount(() => {
+	console.log('[name]: ', props.name);
+})
+</script>
+<template>
+	<div>{{ name }}</div>
+</template>
